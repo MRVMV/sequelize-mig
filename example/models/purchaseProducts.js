58f6ee951f7116e051c32a4091d013bb608cfc1b
MRVMV <mr.v.m.v.x@gmail.com>
2020-09-22 20:42:31 +0300

    test: eslint example models

--- example/models/purchaseProducts.js
+++ example/models/purchaseProducts.js
@@ -2,20 +2,19 @@
 
 // @fix: https://github.com/flexxnn/sequelize-auto-migrations/issues/21
 
-module.exports = function (DB, { INTEGER, DECIMAL }) {
-    const purchaseProducts = DB.define('purchaseProducts', {
-        id: {
-          type: INTEGER.UNSIGNED,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        price: {
-          type: DECIMAL(6, 2),
-          allowNull: false,
-        },
-    });
-    
-    return purchaseProducts;
-  }
-  
+module.exports = (DB, { INTEGER, DECIMAL }) => {
+  const purchaseProducts = DB.define('purchaseProducts', {
+    id: {
+      type: INTEGER.UNSIGNED,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    price: {
+      type: DECIMAL(6, 2),
+      allowNull: false,
+    },
+  });
+
+  return purchaseProducts;
+};
