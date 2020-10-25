7c2082c741022e6f0e8fb7c4d9ad6f4fb0fb2487
MRVMV <mr.v.m.v.x@gmail.com>
2020-10-25 21:39:10 +0300

    test: add pathConfig tests

---
+++ test/helpers.test.js
@@ -0,0 +1,15 @@
+import { pathConfig } from '../lib/helpers.js';
+
+test('pathConfig', async () => {
+  const options = {
+    pwdPath: 'example/example',
+    ignoreSequelizerc: true,
+  };
+  expect(pathConfig(options)).toStrictEqual({
+    modelsDir: 'example\\example\\models',
+    migrationsDir: 'example\\example\\migrations',
+    stateDir: 'example\\example\\models',
+    indexDir: 'example\\example\\models\\index.js',
+    packageDir: 'example\\example\\package.json',
+  });
+});
